4fcbccd81d5c808064a970eda23ffe4a93b92314
Kirissuno <arfilip.1h@gmail.com>
2019-09-27 12:44:55 +0200

    7yMedio terminado

--- 7yMedia/script.js
+++ 7yMedia/script.js
@@ -4,6 +4,8 @@ let tipo = ["Bastos", "Copas", "Espadas", "Oros"];
 let totaljugador = 0;
 let totalbanco = 0;
 
+let arraycombinaciones = [];
+
 const numerosacado = () => {
     return baraja[Math.floor(Math.random()*baraja.length)];
 }
@@ -16,14 +18,118 @@ const pedircarta = document.getElementById("cartaabajojugador");
 
 pedircarta.addEventListener('click', (event) => {
 
-    let imagen = `imagenes/`+numerosacado()+tiposacado()+`.jpg`;
+    let numero = numerosacado();
+    let tipo = tiposacado();
 
-    document.getElementById("cartajugador").src = imagen;
-    let arrayjugador = document.getElementById("arraycartasjugador");
-    let cartajugada = document.createElement("img");
-    cartajugada.src = imagen;
+    if(totaljugador < 7.5){
+        
+    
+        if(!arraycombinaciones.includes(numero+tipo)){
 
-    arrayjugador.appendChild(cartajugada);
+            if(numero > 7){
+                totaljugador+=0.5;
+            }else{
+                totaljugador+=numero;
+            }
+
+            let imagen = `imagenes/`+numero+tipo+`.jpg`;
+    
+            document.getElementById("cartajugador").src = imagen;
+            let arrayjugador = document.getElementById("arraycartasjugador");
+            let cartajugada = document.createElement("img");
+            cartajugada.src = imagen;
+        
+            let puntosjugador = document.getElementById("puntosjugador");
+            puntosjugador.innerHTML = totaljugador;
+        
+            arrayjugador.appendChild(cartajugada);
+    
+            baraja.splice( baraja.indexOf(numero),1 );
+
+            arraycombinaciones.push(numero+tipo);
+
+            jugadorhaperdido();
+        }
+
+    }  
 
 });
 
+const btnplantar = document.getElementById("btnplantar");
+
+btnplantar.addEventListener('click', () => {
+    document.getElementById("pedircartajugador").src = "imagenes/trasera.jpg";
+    let salir = false;
+
+    while(!salir){
+        let numero = numerosacado();
+        let tipo = tiposacado();
+        if(totalbanco < totaljugador && totalbanco < 7.5){
+            if(!arraycombinaciones.includes(numero+tipo)){
+
+                if(numero > 7){
+                    totalbanco+=0.5;
+                }else{
+                    totalbanco+=numero;
+                }
+    
+                let imagen = `imagenes/`+numero+tipo+`.jpg`;
+        
+                document.getElementById("cartabanca").src = imagen;
+                let arraybanca = document.getElementById("arraycartasbanca");
+                let cartajugadabanca = document.createElement("img");
+
+                cartajugadabanca.src = imagen;
+            
+                let puntosbanca = document.getElementById("puntosbanca");
+                puntosbanca.innerHTML = totalbanco;
+            
+                arraybanca.appendChild(cartajugadabanca);
+        
+                baraja.splice( baraja.indexOf(numero),1 );
+    
+                arraycombinaciones.push(numero+tipo);
+            }
+        }else{
+            salir = true;
+        }ganador();
+    }
+    
+});
+
+
+
+const jugadorhaperdido = () => {
+    
+    if(totaljugador > 7.5){
+        document.getElementById("pedircartajugador").src = "imagenes/trasera.jpg";
+        document.getElementById("btnplantar").disabled = true;
+        ganador();
+    }
+    
+
+}
+
+let refresh = document.getElementById("reiniciar");
+
+refresh.addEventListener("click", () => {
+    window.location.reload();
+});
+
+const ganador = () => {
+    let banca = "Ha ganado la banca";
+    let jugador = "Has ganado!";
+    let ganador = document.getElementById("ganador");
+
+    if(totaljugador <= 7.5){
+        if(totalbanco > totaljugador){
+            ganador.innerHTML = jugador;
+        }else if(totalbanco == totaljugador){
+            ganador.innerHTML = banca;
+        }else{
+            ganador.innerHTML = banca;
+        }
+    }else{
+        ganador.innerHTML = banca;
+    }
+}
